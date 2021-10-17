c50bd284057e0b97c5e85893d0c4025444e63010
Dharmil Parekh <dharmilp03@gmail.com>
2021-10-17 18:15:49 +0100

    feat(tester): recording all the http timings and reducing library dependencies

--- engine/tester.js
+++ engine/tester.js
@@ -1,46 +1,45 @@
-const axios = require('axios');
+const https = require('https');
 const moment = require('moment');
 const _ = require('underscore');
+const argv = require('minimist')(process.argv.slice(2));
 
-axios.interceptors.request.use(function(config) {
-    config.time = {
-        reqTime: moment(new Date())
+(async() => {
+    const timings = {
+        startAt: process.hrtime(),
+        dnsLookup: undefined,
+        tcpConnectionAt: undefined,
+        tlsConnectionAt: undefined,
+        firstByteAt: undefined,
+        endAt: undefined
     }
-    return config;
-}, function(error) {
-    console.error('ERROR')
-    console.log(error);
-})
-
-axios.interceptors.response.use(function(response) {
-    _.extend(response.config.time, { resTime: moment(new Date()) })
-    return response;
-}, function(error) {
-    console.error('ERROR')
-    console.log(error);
-    return error;
-})
-
-function triggerUrl(method, url) {
-    this.method = method;
-    this.url = url;
-    return new Promise((resolve, reject) => {
-        axios[method](url)
-            .then((response) => {
-                resolve({
-                    timeTaken: moment.duration(response.config.time.resTime.diff(response.config.time.reqTime)).asMilliseconds(),
-                    success: true
-                })
-            })
-            .catch((err) => {
-                console.log(`${err} some error occured`);
-                reject({
-                    success: false
-                })
-            })
+    var options = {
+        host: argv.url,
+        port: 443,
+        path: argv.path,
+        method: argv.method
+    };
+    const req = https.request(options, (res) => {
+        res.on('data', () => {})
+        res.on('end', () => {
+            timings.endAt = process.hrtime();
+            process.stdout.write(JSON.stringify(timings));
+            process.exitCode = 0;
+        })
+    });
+    req.on('socket', (socket) => {
+        socket.on('lookup', () => {
+            timings.dnsLookupAt = process.hrtime()
+        })
+        socket.on('connect', () => {
+            timings.tcpConnectionAt = process.hrtime()
+        })
+        socket.on('secureConnect', () => {
+            timings.tlsHandshakeAt = process.hrtime()
+        })
+    });
+    req.on('error', function(error) {
+        process.stdout.write(JSON.stringify(error));
+        process.exitCode = 1;
     })
-}
-
-module.exports = {
-    triggerUrl
-}
+    req.end();
+})();
