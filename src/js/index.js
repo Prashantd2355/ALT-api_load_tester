ce7a93b1decc896a51460c3023198f45bc970b83
Kishor Bikram Oli <olikishor@outook.com>
2021-10-24 22:39:20 +0100

    [ALT-2]: Added project files and implement parsing of EXCEL file

---
+++ src/js/index.js
@@ -0,0 +1,36 @@
+(function() {
+    document.getElementById('upload').addEventListener('change', handeSelectedFile, false);
+
+    function handeSelectedFile(evt) {
+        var files = evt.target.files;
+        var excelToJson = new ExcelToJSON();
+        excelToJson.parseExcel(files[0]);
+    }
+
+    var ExcelToJSON = function() {
+
+        this.parseExcel = function(file) {
+
+            debugger;
+            var reader = new FileReader();
+
+            reader.onload = function(e) {
+                var data = e.target.result;
+                var workbook = XLSX.read(data, {
+                    type: 'binary'
+                });
+                workbook.SheetNames.forEach(function(sheetName) {
+                    var XL_row_object = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
+                    var json_object = JSON.stringify(XL_row_object);
+                    console.log(JSON.parse(json_object));
+                })
+            };
+
+            reader.onerror = function(ex) {
+                console.log(ex);
+            };
+
+            reader.readAsBinaryString(file);
+        };
+    };
+})();
