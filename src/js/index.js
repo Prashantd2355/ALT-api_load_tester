d39cdc42fbec49e09993a33bb696e0afbe31b1c6
Kishor Bikram Oli <olikishor@outook.com>
2021-10-26 23:12:25 +0100

    [ALT-2]: Add support for JSON files

--- src/js/index.js
+++ src/js/index.js
@@ -10,27 +10,34 @@
     var ExcelToJSON = function() {
 
         this.parseExcel = function(file) {
-
             debugger;
-            var reader = new FileReader();
+            if (file.type === 'application/vnd.ms-excel') {
+                var reader = new FileReader();
 
-            reader.onload = function(e) {
-                var data = e.target.result;
-                var workbook = XLSX.read(data, {
-                    type: 'binary'
+                reader.onload = function(e) {
+                    var data = e.target.result;
+                    var workbook = XLSX.read(data, {
+                        type: 'binary'
+                    });
+                    workbook.SheetNames.forEach(function(sheetName) {
+                        var XL_row_object = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
+                        var json_object = JSON.stringify(XL_row_object);
+                        console.log(JSON.parse(json_object));
+                    })
+                };
+
+                reader.onerror = function(ex) {
+                    console.log(ex);
+                };
+
+                reader.readAsBinaryString(file);
+            } else {
+                var reader = new FileReader();
+                reader.addEventListener('load', function() {
+                    var result = JSON.parse(reader.result);
                 });
-                workbook.SheetNames.forEach(function(sheetName) {
-                    var XL_row_object = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
-                    var json_object = JSON.stringify(XL_row_object);
-                    console.log(JSON.parse(json_object));
-                })
-            };
-
-            reader.onerror = function(ex) {
-                console.log(ex);
-            };
-
-            reader.readAsBinaryString(file);
+                reader.readAsText(upload.files[0]);
+            }
         };
     };
 })();
