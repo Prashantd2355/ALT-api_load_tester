e833f269e124ab773c661d13db8d468504b9e607
rajputruchi12 <rajputruchi12@gmail.com>
2021-11-17 18:38:07 +0000

    add menu-ui color changes - Ruchi

--- src/renderer/Nav.tsx
+++ src/renderer/Nav.tsx
@@ -1,6 +1,6 @@
-import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFileUpload, faCogs, faChartLine, faInfo } from '@fortawesome/free-solid-svg-icons'
+import { Link,NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFileUpload, faCogs, faChartLine, faInfo } from '@fortawesome/free-solid-svg-icons';
 
 export default function Nav() {
   return (
@@ -12,19 +12,29 @@ export default function Nav() {
         <ul className="list">
         <Link to="/file">
           <li className="list-item">
-             <FontAwesomeIcon icon={faFileUpload} size={'lg'} style={{width: '40px'}}/> <span> Add File</span>
+            <NavLink exact activeClassName="active" to='/file' >
+             <FontAwesomeIcon icon={faFileUpload} size={'lg'} style={{width: '40px'}}/><span> Add File</span>
+            </NavLink>
           </li>
           </Link>
           <Link to="/process">
             <li className="list-item">
-              <FontAwesomeIcon icon={faCogs} size={'lg'} style={{width: '40px'}}/> <span> Process File</span>
+              <NavLink exact activeClassName="active" to='/process' >
+                <FontAwesomeIcon icon={faCogs} size={'lg'} style={{width: '40px'}}/><span> Process File</span>
+              </NavLink>
+            </li>
+          </Link>
+          <Link to="/graph">
+            <li className="list-item">
+              <NavLink exact activeClassName="active" to='/graph' >
+                <FontAwesomeIcon icon={faChartLine} size={'lg'} style={{width: '40px'}}/><span> Graph</span>
+              </NavLink>
             </li>
           </Link>
           <li className="list-item">
-             <FontAwesomeIcon icon={faChartLine} size={'lg'} style={{width: '40px'}}/> <span> Graph</span>
-          </li>
-          <li className="list-item">
-            <FontAwesomeIcon icon={faInfo} size={'lg'} style={{width: '40px'}}/> <span> Help</span>
+            <NavLink exact activeClassName="active" to='/help' >
+              <FontAwesomeIcon icon={faInfo} size={'lg'} style={{width: '40px'}}/><span> Help</span>
+            </NavLink>
           </li>
         </ul>
       </div>
