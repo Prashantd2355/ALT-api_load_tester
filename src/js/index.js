b0f5da481e60f576dc9b729b7b17d5ae9cdeb094
Kishor Bikram Oli <olikishor@outook.com>
2021-11-18 13:00:13 +0000

    [ALT-2]: Added report generation and validation

--- src/js/index.js
+++ src/js/index.js
@@ -10,8 +10,8 @@
     var ExcelToJSON = function() {
 
         this.parseExcel = function(file) {
-            debugger;
-            if (file.type === 'application/vnd.ms-excel') {
+            var methodList = ['GET', 'POST', 'PUT', 'DELETE'];
+            if (file.type === 'application/vnd.ms-excel' || file.type === 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet') {
                 var reader = new FileReader();
 
                 reader.onload = function(e) {
@@ -19,10 +19,28 @@
                     var workbook = XLSX.read(data, {
                         type: 'binary'
                     });
+
+                    //looping each row from excel sheet
                     workbook.SheetNames.forEach(function(sheetName) {
+                        debugger;
                         var XL_row_object = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
                         var json_object = JSON.stringify(XL_row_object);
-                        console.log(JSON.parse(json_object));
+                        var arr = JSON.parse(json_object);
+
+                        //loop each record
+                        arr.forEach(eachElement => {
+                            var url = eachElement.URL;
+                            var method = eachElement.METHOD;
+                            var header = eachElement.HEADERS;
+                            var body = eachElement.BODY;
+
+                            if (!isValidURL(url)) {
+                                alert('URL not valid: ' + url);
+                            }
+                            if (!methodList.includes(method)) {
+                                alert('Method name not valid: ' + method);
+                            }
+                        });
                     })
                 };
 
@@ -31,13 +49,93 @@
                 };
 
                 reader.readAsBinaryString(file);
-            } else {
+            } else if (file.type === 'application/json') {
                 var reader = new FileReader();
                 reader.addEventListener('load', function() {
-                    var result = JSON.parse(reader.result);
+                    var arr = JSON.parse(reader.result);
+
+                    //loop each record
+                    arr.forEach(eachElement => {
+
+                    });
                 });
                 reader.readAsText(upload.files[0]);
+            } else {
+                alert('Unsupported file type!')
             }
         };
     };
 })();
+
+//generates and downloads EXCEL files
+function generateExcelFile() {
+    debugger;
+    var wb = XLSX.utils.book_new();
+    wb.Props = {
+        Title: "ALT Excel File",
+        Subject: "Sample Excel File for ALT",
+        Author: "ALT-Group-UEL"
+    };
+    wb.SheetNames.push("Sheet");
+
+    var ws_data = [
+        ['URL', 'METHOD', 'HEADERS', 'BODY']
+    ]; //a row with 4 columns
+
+    var ws = XLSX.utils.aoa_to_sheet(ws_data);
+
+    wb.Sheets["Sheet"] = ws;
+    var wbout = XLSX.write(wb, { bookType: 'xlsx', type: 'binary' });
+
+    saveAs(new Blob([s2ab(wbout)], { type: "application/octet-stream" }), 'ALT.xlsx');
+}
+
+//generates and downloads JSON files
+function generateJSONFile() {
+    var apiJSONFormat = [{
+            'URL': 'https://www.example.com/getUsers/',
+            'METHOD': 'GET',
+            'HEADERS': '{}',
+            'BODY': '{}'
+        },
+        {
+            'URL': 'https://www.example.com/createUsers/',
+            'METHOD': 'POST',
+            'HEADERS': '{}',
+            'BODY': '{}'
+        }
+    ];
+    var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(apiJSONFormat));
+    var downloadAnchorNode = document.createElement('a');
+    downloadAnchorNode.setAttribute("href", dataStr);
+    downloadAnchorNode.setAttribute("download", "ALT.json");
+    document.body.appendChild(downloadAnchorNode); // required for firefox
+    downloadAnchorNode.click();
+    downloadAnchorNode.remove();
+}
+
+
+
+/** HELPER METHODS STARTS HERE */
+
+function s2ab(s) {
+    var buf = new ArrayBuffer(s.length); //convert s to arrayBuffer
+    var view = new Uint8Array(buf); //create uint8array as viewer
+    for (var i = 0; i < s.length; i++) view[i] = s.charCodeAt(i) & 0xFF; //convert to octet
+    return buf;
+}
+
+
+//method to validate URLs
+function isValidURL(string) {
+    var res = string.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+    return (res !== null)
+};
+
+
+
+function validateMethods(methodName) {
+
+}
+
+/** HELPER METHODS ENDS HERE */
